b4316752f50617e77fe2a74d41ca13446c24fece
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-04-02 11:41:16 +0200

    Up and down

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ document.addEventListener("keydown", function (event) {
   const dodger = document.getElementById("dodger");
   const gameContainer = dodger.parentElement;
 
-
   function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -30,10 +29,40 @@ document.addEventListener("keydown", function (event) {
     }
   }
 
+function moveDodgerUp() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+
+    const containerHeight = gameContainer.clientHeight;
+    const dodgerHeight = dodger.clientHeight;
+
+    if (bottom < containerHeight - dodgerHeight) {
+        dodger.style.bottom = `${bottom + 10}px`;
+    }
+}
+
+function moveDodgerDown() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+
+    const containerHeight = gameContainer.clientHeight;
+    const dodgerHeight = dodger.clientHeight;
+
+    if (bottom < containerHeight - dodgerHeight) {
+        dodger.style.bottom = `${bottom - 10}px`;
+    }
+}
+
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
-    } if (e.key === "ArrowRight") {
+    } else if (e.key === "ArrowRight") {
         moveDodgerRight();
-      }
+    } else if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    } else if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
   });
+
+  
