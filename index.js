75c375b71010a8ec4c77110e3c15b31316086b69
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-04-02 11:07:53 +0200

    No more out of bounds on the right

--- index.js
+++ index.js
@@ -5,32 +5,28 @@ document.addEventListener("keydown", function (event) {
     console.log(event);
   });
 
-// document.addEventListener("keydown", function (event) {
-//     if (event.key === "ArrowLeft") {
-//       const leftNumbers = dodger.style.left.replace("px", "");
-//       const left = parseInt(leftNumbers, 10);
-  
-//       dodger.style.left = `${left - 1}px`;
-//     }
-//   });
-
   const dodger = document.getElementById("dodger");
+  const gameContainer = dodger.parentElement;
+
 
   function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 10}px`;
     }
   }
 
   function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
+
+    const containerWidth = gameContainer.clientWidth;
+    const dodgerWidth = dodger.clientWidth;
   
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
+    if (left + dodgerWidth < containerWidth) {
+      dodger.style.left = `${left + 10}px`;
     }
   }
 
