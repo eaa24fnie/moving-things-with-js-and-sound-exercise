be3ecf7786e709a03f5b44b7d5c506612aed2f8e
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-04-02 10:59:05 +0200

    Move Right

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ document.addEventListener("keydown", function (event) {
     console.log(event);
   });
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-      const leftNumbers = dodger.style.left.replace("px", "");
-      const left = parseInt(leftNumbers, 10);
+// document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowLeft") {
+//       const leftNumbers = dodger.style.left.replace("px", "");
+//       const left = parseInt(leftNumbers, 10);
   
-      dodger.style.left = `${left - 1}px`;
-    }
-  });
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   });
 
   const dodger = document.getElementById("dodger");
 
@@ -24,3 +24,20 @@ document.addEventListener("keydown", function (event) {
       dodger.style.left = `${left - 1}px`;
     }
   }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
